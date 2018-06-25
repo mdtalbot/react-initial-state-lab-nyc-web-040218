4f282c314c922629d91b71082278d10ccd929c52
Matthew Talbot <mdtalbot@gmail.com>
2018-06-25 17:11:57 -0400

    Done.

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -1,1 +1,31 @@
 // Bomb Component Code Goes Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {};
+
+const defaultProps = {};
+
+export default class Bomb extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      secondsLeft: this.props.initialCount
+    };
+  }
+
+  render() {
+    if (this.state.secondsLeft > 0) {
+      return (
+
+        <p>{this.state.secondsLeft} seconds left before I go boom!</p>
+      )
+    }
+    else {
+      return <h2>Boom!</h2>
+    }
+  }
+}
+
+ Bomb.propTypes = propTypes;
+ Bomb.defaultProps = defaultProps;
--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,1 +1,25 @@
 // Bomb Component Code Goes Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {};
+
+const defaultProps = {};
+
+export default class ImageSlider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentSlideIndex: 0
+    };
+  }
+
+  render() {
+    return (
+      <p>I am on slide {this.state.currentSlideIndex}</p>
+    );
+  }
+}
+
+ ImageSlider.propTypes = propTypes;
+ ImageSlider.defaultProps = defaultProps;
